4f46bcc55c1f18d6e4a4354a99052ade3c6cc361
krizsa <gabor.krizsanits@gmail.com>
2011-06-06 15:10:21 +0200

    build script for node-ssh

---
+++ build.sh
@@ -0,0 +1,9 @@
+git submodule update --init --recursive
+cd support/libssh/build
+cmake -DCMAKE_INSTALL_PREFIX=/usr/local -DCMAKE_BUILD_TYPE=Debug ..
+make install
+PKG_CONFIG_PATH=/usr/local/lib/pkgconfig
+export PKG_CONFIG_PATH
+cd ../../..
+node-waf configure
+node-waf build
